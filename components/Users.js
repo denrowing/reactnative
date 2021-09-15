e6b993dca49bb9f97ca17fcd7df286ceb2b6f401
Denys Burdin <denrowing@gmail.com>
2021-09-15 10:58:57 +0300

    init commit

---
+++ components/Users.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {StyleSheet} from 'react-native'
+import {View, Text, Button, TouchableOpacity} from 'react-native'
+
+const x = () => {
+  return <View>
+        <Text></Text>
+  </View>
+}
+
+export default x
+
+let styles = StyleSheet.create({})
