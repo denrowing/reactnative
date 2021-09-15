8a5daed01d87c0dcc390fc7ed4f5ef00984b74e2
Denys Burdin <denrowing@gmail.com>
2021-09-15 21:49:27 +0300

    navigate 1

--- screens/UserDetails.js
+++ screens/UserDetails.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {StyleSheet} from 'react-native'
 import {View, Text, Button, TouchableOpacity} from 'react-native'
 
+
 const UserDetails = (props) => {
     console.log(props);
     let {route: {params: {data}}, navigation} = props
